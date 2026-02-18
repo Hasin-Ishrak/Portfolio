c7fb1c00efec2ecb2005c83e948d1ece4d34421e
Hasin-Ishrak <ishrakhasin758@gmail.com>
2026-02-18 18:09:33 +0600

    added light mode

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 const currentPage = window.location.pathname.split("/").pop();
 const navLinks = document.querySelectorAll("nav ul li a");
 
-navLinks.forEach(link => {
+navLinks.forEach((link) => {
   if (link.getAttribute("href") === currentPage) {
     link.classList.add("active");
   }
 });
+const themeToggle = document.getElementById("themeToggle");
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light-mode");
+  themeToggle.textContent = "☀️";
+}
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+  if (document.body.classList.contains("light-mode")) {
+    localStorage.setItem("theme", "light");
+    themeToggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "dark");
+    themeToggle.textContent = "🌙";
+  }
+});
 const typedElement = document.querySelector("#element");
-
 if (typedElement) {
   new Typed("#element", {
     strings: [
       "Web Developer",
       "Competitive Programmer",
       "Problem Solver",
-      "Software Engineer"
+      "Software Engineer",
     ],
     typeSpeed: 60,
     backSpeed: 40,
@@ -23,10 +37,38 @@ if (typedElement) {
     loop: true,
     smartBackspace: true,
     showCursor: true,
-    cursorChar: "|"
+    cursorChar: "|",
   });
 }
 const year = document.getElementById("year");
 if (year) {
   year.textContent = new Date().getFullYear();
 }
+const counter = document.getElementById("problemCount");
+function animateCounter() {
+  let count = 0;
+  const target = 1600;
+  const speed = 2;
+  const updateCounter = () => {
+    if (count < target) {
+      count += 10;
+      counter.textContent = count;
+      setTimeout(updateCounter, speed);
+    } else {
+      counter.textContent = target;
+    }
+  };
+
+  updateCounter();
+}
+window.addEventListener("scroll", () => {
+  if (counter) {
+    const position = counter.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100 && !counter.classList.contains("counted")) {
+      counter.classList.add("counted");
+      animateCounter();
+    }
+  }
+});
